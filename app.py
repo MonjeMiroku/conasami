671696a95972e055f9bbd2d3d885f54705fbad93
MonjeMiroku <hiramlatorre@hotmail.com>
2023-01-20 23:23:00 -0700

    new plotly app

--- app.py
+++ app.py
@@ -1,13 +1,30 @@
-import dash
-import dash_bootstrap_components as dbc
+from dash import Dash, dcc, html, Input, Output
+import plotly.graph_objects as go
 
-app = dash.Dash(
-    external_stylesheets=[dbc.themes.BOOTSTRAP]
-)
+app = Dash(__name__)
 
-app.layout = dbc.Alert(
-    "Hello, Bootstrap!", className="m-5"
-)
 
-if __name__ == "__main__":
-    app.run_server()
+app.layout = html.Div([
+    html.H4('Interactive color selection with simple Dash example'),
+    html.P("Select color:"),
+    dcc.Dropdown(
+        id="dropdown",
+        options=['Gold', 'MediumTurquoise', 'LightGreen'],
+        value='Gold',
+        clearable=False,
+    ),
+    dcc.Graph(id="graph"),
+])
+
+
+@app.callback(
+    Output("graph", "figure"),
+    Input("dropdown", "value"))
+def display_color(color):
+    fig = go.Figure(
+        data=go.Bar(y=[2, 3, 1], # replace with your own data source
+                    marker_color=color))
+    return fig
+
+
+app.run_server(debug=True)
