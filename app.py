150d7986af62794d8e7362cb5a7ee31451d675c2
MonjeMiroku <hiramlatorre@hotmail.com>
2023-01-20 23:08:05 -0700

    updated flask

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask
-app = Flask(__name__)
+import dash
+import dash_bootstrap_components as dbc
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+app = dash.Dash(
+    external_stylesheets=[dbc.themes.BOOTSTRAP]
+)
+
+app.layout = dbc.Alert(
+    "Hello, Bootstrap!", className="m-5"
+)
+
+if __name__ == "__main__":
+    app.run_server()
